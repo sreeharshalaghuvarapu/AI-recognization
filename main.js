d795733152b7f7e89426e81a485b3d6c4c8d8347
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2022-01-16 18:53:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+    noseY=0;
+    difference = 0;
+    rightWristX = 0;
+    leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO)
     video.size(550, 500);
@@ -23,7 +29,36 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
     }
+}
+function draw(){
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "width and height of a square will be = " + difference +"px"
+    fill('#F90093')
+    stroke('#F90093')
+    square(noseX, noseY, difference);
+    
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX +" noseY = " + noseY);
+    
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference =floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightwristX = "+ rightWristX + "difference = " + difference);
+    }
+    
 }
 
 
